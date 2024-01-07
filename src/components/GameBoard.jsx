feb10480f2def687f28c19fcbbfe9708e98129a9
Ardiantohendy <bayuardianto421@gmail.com>
2024-01-07 23:40:21 +0700

    new commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,11 @@
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function GameBoard() {
+  return <ol id="game-board"></ol>;
+}
+
+export default GameBoard;
